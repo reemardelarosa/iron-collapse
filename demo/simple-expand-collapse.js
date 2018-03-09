11719078791e520b4136dad75265c6d7238a5639
Elliott Marquez <emarquez@google.com>
2018-03-08 16:08:34 -0800

    auto-converted by polymer-modulizer

--- demo/simple-expand-collapse.js
+++ demo/simple-expand-collapse.js
@@ -1,4 +1,8 @@
-<!--
+import '../iron-collapse.js';
+import '../../paper-styles/shadow.js';
+import { Polymer } from '../../polymer/lib/legacy/polymer-fn.js';
+import { html } from '../../polymer/lib/utils/html-tag.js';
+/**
 @license
 Copyright (c) 2016 The Polymer Project Authors. All rights reserved.
 This code may only be used under the BSD style license found at http://polymer.github.io/LICENSE.txt
@@ -6,15 +10,9 @@ The complete set of authors may be found at http://polymer.github.io/AUTHORS.txt
 The complete set of contributors may be found at http://polymer.github.io/CONTRIBUTORS.txt
 Code distributed by Google as part of the polymer project is also
 subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
--->
-
-<link rel="import" href="../iron-collapse.html">
-<link rel="import" href="../../paper-styles/shadow.html">
-
-<dom-module id="simple-expand-collapse">
-
-  <template>
-
+*/
+Polymer({
+  _template: html`
     <style>
 
       :host {
@@ -45,41 +43,33 @@ subject to an additional IP rights grant found at http://polymer.github.io/PATEN
         @apply --shadow-elevation-2dp;
       }
     </style>
-    <button id="trigger" on-click="toggle" aria-expanded$="[[opened]]" aria-controls="collapse">[[_getText(opened)]]</button>
+    <button id="trigger" on-click="toggle" aria-expanded\$="[[opened]]" aria-controls="collapse">[[_getText(opened)]]</button>
     <iron-collapse id="collapse" opened="{{opened}}" horizontal="[[horizontal]]" no-animation="[[noAnimation]]" tabindex="0">
       <slot></slot>
     </iron-collapse>
+`,
 
-  </template>
+  is: 'simple-expand-collapse',
 
-</dom-module>
+  properties: {
 
-<script>
-  Polymer({
-
-    is: 'simple-expand-collapse',
-
-    properties: {
-
-      horizontal: {
-        type: Boolean
-      },
-      opened: {
-        type: Boolean,
-        reflectToAttribute: true
-      },
-      noAnimation: {
-        type: Boolean
-      },
+    horizontal: {
+      type: Boolean
     },
-
-    toggle: function() {
-      this.$.collapse.toggle();
+    opened: {
+      type: Boolean,
+      reflectToAttribute: true
     },
+    noAnimation: {
+      type: Boolean
+    },
+  },
 
-    _getText: function(opened) {
-      return opened ? 'Collapse' : 'Expand';
-    }
+  toggle: function() {
+    this.$.collapse.toggle();
+  },
 
-  });
-</script>
+  _getText: function(opened) {
+    return opened ? 'Collapse' : 'Expand';
+  }
+});
